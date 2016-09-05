69e4c94636001aed460690329997cde13bcc3209
nirco77 <nirco77@gmail.com>
2016-09-05 15:35:45 +0300

    .

--- day_1_ex_1/src/main/java/spring/course/exercise_1/model/Ship.java
+++ day_1_ex_1/src/main/java/spring/course/exercise_1/model/Ship.java
@@ -11,6 +11,14 @@ public class Ship {
 
     private Engine engine;
 
+    public void happyInit() {
+        System.out.println("I'm Alive !");
+    }
+
+    public void sadInit() {
+        System.out.println("Oh No !");
+    }
+
     public void sayMyName(){
         System.out.println("my engine is : " + engine.sayMyName());
     }
